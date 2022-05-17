92d823402a84ecb08768203070e0960b5559d27e
sofianechaghla <77396241+sofianechaghla@users.noreply.github.com>
2022-05-18 00:58:44 +0200

    améliore la structure de twig avec l'héritage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,20 +11,19 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main_home")
      */
-
-    public function home()
+    public function home ()
     {
-        echo "coucou";
-        die();
+        return $this->render("main/home.html.twig");
     }
 
     /**
-     * @Route("/test", name="main_home")
+     * @Route("/test", name="main_test")
      */
-
     public function test()
     {
-        echo "testounet";
-        die();
+        return $this->render('main/test.html.twig');
     }
 }
+
+
+
